0e75cba50976f03b4aec710b5e525ddbe0c90fce
Claudiu Tirisi <littleclasher88@gmail.com>
2025-11-16 23:51:54 +0200

    Extract info from the request and prepare for saving to db

--- server/src/request.rs
+++ server/src/request.rs
@@ -1,6 +1,9 @@
+use crate::db;
+use crate::schema::tunnels::dsl::*;
 use axum::{
     body::Bytes,
     extract::ConnectInfo,
+    extract::State,
     http,
     http::{Method, StatusCode, Uri},
     response::IntoResponse,
@@ -9,7 +12,7 @@ use diesel::prelude::*;
 use serde_json::Value;
 use std::net::SocketAddr;
 
-#[derive(Insertable)]
+#[derive(Insertable, Debug)]
 #[diesel(table_name = crate::schema::webhook_requests)]
 struct NewWebhookRequest {
     tunnel_id: i32,
@@ -23,32 +26,68 @@ struct NewWebhookRequest {
 }
 
 pub async fn request_handler(
+    State(db_conn): State<db::DbPool>,
     method: Method,
     uri: Uri,
     headers: http::HeaderMap,
     ConnectInfo(remote_addr): ConnectInfo<SocketAddr>,
     body: Bytes,
 ) -> impl IntoResponse {
-    let method = method.as_str();
-    let path_and_query = uri.to_string();
-    println!("Method: {}", method);
-    println!("URI: {}", path_and_query);
-    println!("Headers: {:?}", headers);
+    let mut conn = db_conn.get().expect("Couldn't get db connection");
 
-    let body_string = match std::str::from_utf8(&body) {
-        Ok(s) => s,
-        Err(_) => "[Binary or Invalid UTF-8 Body] test",
+    let method = method.as_str().to_owned();
+    let path_and_query = uri.to_string();
+    let remote_ip_value = headers
+        .get("x-real-ip")
+        .expect("x-real-ip header is missing, should have been added by nginx");
+    let remote_ip_str = remote_ip_value.to_str().expect("x-real-ip to_str() failed");
+    let remote_ip = remote_ip_str.to_owned();
+    let remote_port = remote_addr.port();
+
+    let host = headers.get("host").expect("host header is missing");
+    let host_str = host.to_str().expect("host header to_str() failed");
+    let host = host_str.to_owned();
+
+    let subdomain = match host.split(".").next() {
+        Some(s) => s.to_owned(),
+        None => {
+            return (StatusCode::BAD_REQUEST, "400 Bad Request: No such tunnel.").into_response();
+        }
     };
 
-    println!("Body length: {}", body.len());
-    println!("Body Content: {}", body_string);
+    let subdomain = subdomain.to_lowercase();
 
-    let remote_port = remote_addr.port();
-    println!("Source port: {}", remote_port);
+    println!("!{}!", subdomain);
+    let tunnel_id = tunnels
+        .filter(tunnel_name.eq(subdomain))
+        .select(id)
+        .first::<i32>(&mut conn)
+        .optional()
+        .expect("Db query for looking for tunnel id failed");
 
-    (
-        StatusCode::BAD_REQUEST,
-        "400 Bad Request: Unhandled endpoint or method.",
-    )
-        .into_response()
+    let tunnel_id: i32 = match tunnel_id {
+        Some(t_id) => t_id,
+        None => {
+            return (
+                StatusCode::NOT_FOUND,
+                "404 Not Found: Tunnel ID could not be found for the given subdomain.",
+            )
+                .into_response();
+        }
+    } as i32;
+
+    let webhook_request = NewWebhookRequest {
+        tunnel_id: tunnel_id,
+        method: method,
+        path: path_and_query,
+        headers: diesel_json::Json(headers),
+        remote_port: remote_port as i16,
+        remote_ip: remote_ip,
+        body: Some(body.to_vec()),
+        body_length: Some(body.len() as i32),
+    };
+
+    println!("{:?}", webhook_request);
+
+    (StatusCode::OK, "Success.").into_response()
 }
