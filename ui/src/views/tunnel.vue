f4f914264540ebf08bc2f43054bc415f2edc0e47
claudiu7t <littleclasher88@gmail.com>
2026-01-18 21:59:56 +0200

    Add very basic tunnel requests view with SSE

--- ui/src/views/tunnel.vue
+++ ui/src/views/tunnel.vue
@@ -1,15 +1,29 @@
 <template>
   <div class="tunnel-view">
     <h1>Tunnel {{ tunnelId }}</h1>
+    <h2>Events</h2>
+    <div v-for="(event, index) in events" :key="index">
+      {{ event }}
+      <br />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue";
+import { computed, onMounted } from "vue";
 import { useRoute } from "vue-router";
+import { useSSE } from "../sse/sse";
 
 const route = useRoute();
 const tunnelId = computed(() => route.params.id);
+
+const { events, isConnected, error, connect } = useSSE(
+  tunnelId.value as string,
+);
+
+onMounted(() => {
+  connect();
+});
 </script>
 
 <style scoped></style>
