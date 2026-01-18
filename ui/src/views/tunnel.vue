b3515a17a4f337cecff04219373e9ced70abf983
claudiu7t <littleclasher88@gmail.com>
2026-01-18 06:22:17 +0200

    Add tunnel page

---
+++ ui/src/views/tunnel.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="tunnel-view">
+    <h1>Tunnel {{ tunnelId }}</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+const tunnelId = computed(() => route.params.id);
+</script>
+
+<style scoped></style>
